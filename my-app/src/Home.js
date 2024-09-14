c6438a82de081d124992bdad0883085d9cbd9ed0
RobBundy2002 <robbielbundy@Gmail.com>
2024-09-13 23:52:44 -0400

    Home page fix

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -4,6 +4,13 @@ import './Home.css';
 function Home() {
     return (
         <div className="Home">
+            <div className="Home-message">
+                The Undergraduate Journal of Law and Politics is a group of writers, editors, and executives that work
+                to draft and publish undergraduate students’ writings of law reviews, political interests, and opinion
+                pieces. Unlike a traditional law review, the UJLP provides members with the opportunity to write on both
+                law and politics, not just strictly court cases. We publish scholarly articles, essays, book reviews,
+                and commentaries that explore the dynamic relationship between legal frameworks and political processes.
+            </div>
         </div>
     );
 }
