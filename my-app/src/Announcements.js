13822fed28e58f17e10e419cc04cc2ebde933244
Derek <derektsai2006@gmail.com>
2025-01-22 21:51:17 -0500

    Made a new announcement

--- my-app/src/Announcements.js
+++ my-app/src/Announcements.js
@@ -3,7 +3,7 @@ import './Announcements.css';
 
 function Announcements() {
     const announcements = [
-        { title: "Guest Speaker Event", date: "1/24/2025", description:" The UJLP will be hosting a guest speaker event with S.J.D. Candidate and Double Hoo: Yumiao (Michael) Wang!" },
+        { title: "Guest Speaker Event", date: "1/24/2025", description:" The UJLP will be hosting a guest speaker event with S.J.D. Candidate and Double Hoo: Yumiao (Michael) Wang! " },
         { title: "New Member Announcement", date: "10/24/2024", description:" The UJLP formally accepted 10 new writers and 7 new editors for our 2024-2025 publication cycle! " },
     ];
 
